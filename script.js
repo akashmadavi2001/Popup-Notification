629ae290604a9047cd8a08b3b9c652ea93a5f6c9
Akash Madavi <akashmadavi2001@gmail.com>
2025-11-19 09:41:49 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let pop = document.querySelector('.pop');
 
 function show() {
-    pop.classList.add('pop-open')
-}
+    pop.classList.add('pop-open');
+};
 
 function remove() {
-    pop.classList.remove('pop-open')
-}
+    pop.classList.remove('pop-open');
+};
